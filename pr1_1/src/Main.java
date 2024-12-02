a4e8b8870f86a71fb7e254711f82734d5f629852
DanilRumyantsev <danilrumancev48@gmail.com>
2024-12-02 20:20:05 +0300

    'egioeiwio'

---
+++ pr1_1/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int fn = 15;
+        int sn = 10;
+        int sum;
+        int sub;
+        int mult;
+        int div;
+        int remofdiv;
+
+        sum = fn + sn;
+        sub = fn - sn;
+        mult = fn * sn;
+        div = fn / sn;
+        remofdiv = fn % sn;
+        System.out.println("Sum " + sum);
+        System.out.println("Sub " + sub);
+        System.out.println("Mult " + mult);
+        System.out.println("Div " + div);
+        System.out.println("Remofdiv " + remofdiv);
+    }
+}
